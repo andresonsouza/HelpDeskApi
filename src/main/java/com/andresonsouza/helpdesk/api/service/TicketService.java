6fc897677cc8e6494720d51ee264600301b3e49f
Andreson Souza <andresonjazz@gmail.com>
2020-10-07 22:50:10 -0300

    feat: crie o serviço de ticket

--- src/main/java/com/andresonsouza/helpdesk/api/service/TicketService.java
+++ src/main/java/com/andresonsouza/helpdesk/api/service/TicketService.java
@@ -1,59 +1,38 @@
 package com.andresonsouza.helpdesk.api.service;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 import com.andresonsouza.helpdesk.api.entity.ChangeStatus;
 import com.andresonsouza.helpdesk.api.entity.Ticket;
 
+
 @Component
-public class TicketService {
+public interface TicketService {
 
-	Ticket createOrUpdate(Ticket ticket) {
-		return null;
-	}
-
-	Ticket findById(String id) {
-		return null;
-	}
-
-	void delete(String id) {
-	}
-
-	Page<Ticket> listTicket(int page, int count) {
-		return null;
-	}
-
-	ChangeStatus createChangeStatus(ChangeStatus changeStatus) {
-		return changeStatus;
-	}
-
-	Iterable<ChangeStatus> listChangeStatus(String ticketId) {
-		return null;
-	}
-
-	Page<Ticket> findCurrentUser(int page, int count, String userId) {
-		return null;
-	}
-
-	Page<Ticket> findByParameters(int page, int count, String title, String status, String priority) {
-		return null;
-	}
-
-	Page<Ticket> findByParametersAndCurrentUser(int page, int count, String title, String status, String priority,
-			String userId) {
-		return null;
-	}
-
-	Page<Ticket> findByName(int page, int count, Integer number) {
-		return null;
-	}
-
-	Iterable<Ticket> findAll() {
-		return null;
-	}
-
-	Page<Ticket> findParameterAndAssignedUser(int page, int count, String title, String status, String priority, String assignedUser) {
-		return null;
-	}
+	Ticket createOrUpdate(Ticket ticket);
+	
+	Optional<Ticket> findById(String id);
+	
+	void delete(String id);
+	
+	Page<Ticket> listTicket(int page, int count);
+	
+	ChangeStatus createChangeStatus(ChangeStatus changeStatus);
+	
+	Iterable<ChangeStatus> listChangeStatus(String ticketId);
+	
+	Page<Ticket> findByCurrentUser(int page, int count, String userId);
+	
+	Page<Ticket> findByParameters(int page, int count,String title,String status,String priority);
+	
+	Page<Ticket> findByParametersAndCurrentUser(int page, int count,String title,String status,String priority,String userId);
+	
+	Page<Ticket> findByNumber(int page, int count,Integer number);
+	
+	Iterable<Ticket> findAll();
+	
+	public Page<Ticket> findByParametersAndAssignedUser(int page, int count,String title,String status,String priority,String assignedUserId);
 }
